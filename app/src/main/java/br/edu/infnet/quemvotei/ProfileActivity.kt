72b2e05463aaba79ca15304fcc8bfc2757f0cb4c
Hallison Paz <hallison.dapaz@gmail.com>
2019-12-20 17:18:30 -0300

    added code to AcCTION_CALL

--- app/src/main/java/br/edu/infnet/quemvotei/ProfileActivity.kt
+++ app/src/main/java/br/edu/infnet/quemvotei/ProfileActivity.kt
@@ -1,5 +1,7 @@
 package br.edu.infnet.quemvotei
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_profile.*
@@ -10,6 +12,11 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
+        recuperarDados()
+        configurarListeners()
+    }
+
+    fun recuperarDados(){
         val nome = intent.getStringExtra(NOME_EXTRA)
         val cidade = intent.getStringExtra(CIDADE_EXTRA)
         val idade = intent.getStringExtra(IDADE_EXTRA)
@@ -22,6 +29,17 @@ class ProfileActivity : AppCompatActivity() {
             FACULTATIVO -> "Seu voto será FACULTATIVO na próxima eleição"
             OBRIGATORIO -> "Seu voto será OBRIGATÓRIO na próxima eleição"
             else -> "Menor de 16? Como veio parar aqui?"
+        }
+    }
+
+    fun configurarListeners(){
+        discar_button.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL).apply {
+                data = Uri.parse("tel:$08006480005")
+            }
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            }
         }
     }
 }
