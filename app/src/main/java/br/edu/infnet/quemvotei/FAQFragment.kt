040afce345a39e881662ee478760525a888bee99
Hallison Paz <hallison.dapaz@gmail.com>
2019-12-23 18:59:33 -0300

    faq fragment ui complete; mock data in the file.

--- app/src/main/java/br/edu/infnet/quemvotei/FAQFragment.kt
+++ app/src/main/java/br/edu/infnet/quemvotei/FAQFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import br.edu.infnet.quemvotei.dados.duvidas
+import kotlinx.android.synthetic.main.fragment_faq.*
 
 /**
  * A simple [Fragment] subclass.
@@ -20,5 +22,24 @@ class FAQFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_faq, container, false)
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Lista de objetos TextView que exibirão o texto das perguntas
+        val perguntasTextViews = listOf(p1_textview,
+                                        p2_textview,
+                                        p3_textview,
+                                        p4_textview,
+                                        p5_textview)
+        // Lista de objetos TextView que exibirão o texto das respostas
+        val respostasTextViews = listOf(r1_textview,
+                                        r2_textview,
+                                        r3_textview,
+                                        r4_textview,
+                                        r5_textview)
+        // Configura cada texto iterativamente
+        for (i in duvidas.indices){
+            perguntasTextViews[i].text = duvidas[i].pergunta
+            respostasTextViews[i].text = duvidas[i].resposta
+        }
+    }
 }
---
+++ app/src/main/java/br/edu/infnet/quemvotei/modelos/Duvida.kt
@@ -0,0 +1,5 @@
+package br.edu.infnet.quemvotei.modelos
+
+class Duvida(val pergunta: String,
+             val resposta: String,
+             val categoria: String)
